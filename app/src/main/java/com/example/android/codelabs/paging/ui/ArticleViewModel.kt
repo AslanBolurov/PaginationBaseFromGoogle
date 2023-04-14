a077b3a91e4d5b52ace8d8bbb774f2430d80b7da
AslanBolurov <71359713+AslanBolurov@users.noreply.github.com>
2023-04-14 20:46:10 +0300

    PagingDataAdapter+Pager

--- app/src/main/java/com/example/android/codelabs/paging/ui/ArticleViewModel.kt
+++ app/src/main/java/com/example/android/codelabs/paging/ui/ArticleViewModel.kt
@@ -2,20 +2,25 @@ package com.example.android.codelabs.paging.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.example.android.codelabs.paging.data.Article
 import com.example.android.codelabs.paging.data.ArticleRepository
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.*
+
+private const val ITEMS_PER_PAGE = 50
 
 class ArticleViewModel(
     repository: ArticleRepository,
 ) : ViewModel() {
 
-    val items: StateFlow<List<Article>> = repository.articleStream
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(),
-            initialValue = listOf()
-        )
+    val items: Flow<PagingData<Article>> = Pager(
+        config = PagingConfig(pageSize = ITEMS_PER_PAGE, enablePlaceholders = false),
+        pagingSourceFactory = { repository.articlePagingSource() }
+    )
+        .flow
+        .cachedIn(viewModelScope)
+
 }
